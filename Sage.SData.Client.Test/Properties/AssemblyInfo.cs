95fc80f09a0cf937e2c9642897fe7f417b2b0966
SageScottsdalePlatform <jeff.hershauer@sage.com>
2009-05-27 20:50:49 +0000

    [#4343] Auto commit from MSBuild Deploy.
    git-svn-id: http://gforge.sageinternal.com/svn/sdataframeworks/ClientLibraries/CoreLib_CSharp/trunk@458 6f033bd9-c263-0410-80ef-92ddf8b29041

--- Sage.SData.Client.Test/Properties/AssemblyInfo.cs
+++ Sage.SData.Client.Test/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1053")]
-[assembly: AssemblyFileVersion("1.0.0.1053")]
+[assembly: AssemblyVersion("1.0.0.1054")]
+[assembly: AssemblyFileVersion("1.0.0.1054")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: AssemblyInformationalVersion("1.0")]
--- Sage.SData.Client/Properties/AssemblyInfo.cs
+++ Sage.SData.Client/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1053")]
-[assembly: AssemblyFileVersion("1.0.0.1053")]
+[assembly: AssemblyVersion("1.0.0.1054")]
+[assembly: AssemblyFileVersion("1.0.0.1054")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: AssemblyInformationalVersion("1.0")]
 
