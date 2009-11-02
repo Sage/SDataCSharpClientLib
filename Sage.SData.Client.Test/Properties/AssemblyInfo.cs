3c8bf8dbb1488be155e11f8280d07dcf58636e83
SageScottsdalePlatform <jeff.hershauer@sage.com>
2009-11-02 09:12:56 +0000

    [#4343] Auto commit from MSBuild Deploy.
    git-svn-id: http://gforge.sageinternal.com/svn/sdataframeworks/ClientLibraries/CoreLib_CSharp/trunk@687 6f033bd9-c263-0410-80ef-92ddf8b29041

--- Sage.SData.Client.Test/Properties/AssemblyInfo.cs
+++ Sage.SData.Client.Test/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1262")]
-[assembly: AssemblyFileVersion("1.0.0.1262")]
+[assembly: AssemblyVersion("1.0.0.1263")]
+[assembly: AssemblyFileVersion("1.0.0.1263")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: AssemblyInformationalVersion("1.0")]
