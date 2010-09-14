c194e41944e025ab693de432cc53f5815e2e2ab1
Nathan Baulch <nathan.baulch@gmail.com>
2010-09-15 01:41:56 +1000

    [33925] Path segment parser now allows for parentheses characters in string literal predicates

---
+++ Sage.SData.Client.Test/Framework/UriPathParserTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Sage.SData.Client.Framework;
+
+namespace Sage.Common.Syndication.Test.Uri
+{
+    [TestFixture]
+    public class UriPathParserTests
+    {
+        /// <summary>
+        /// The URI path parser should handle open parentheses characters
+        /// in literal string predicates.
+        /// </summary>
+        [Test]
+        public void Literal_String_Predicate_With_Open_Paren_Test()
+        {
+            var segments = UriPathParser.Parse("aaa('bbb(ccc')");
+            Assert.That(segments, Is.Not.Null);
+            Assert.That(segments.Length, Is.EqualTo(1));
+
+            var segment = segments[0];
+            Assert.That(segment.Text, Is.EqualTo("aaa"));
+            Assert.That(segment.HasPredicate);
+            Assert.That(segment.Predicate, Is.EqualTo("'bbb(ccc'"));
+        }
+    }
+}
