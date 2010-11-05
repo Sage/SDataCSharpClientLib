5e9f7ecab1cc70e8ea7e5202c051ee5aa4a464e4
Nathan Baulch <nathan.baulch@gmail.com>
2010-11-05 15:32:53 +1100

    [34658] Unit test for schema properties that don't specify a type

---
+++ Sage.SData.Client.Test/Metadata/SDataSchemaTests.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using NUnit.Framework;
+using Sage.SData.Client.Metadata;
+
+namespace Sage.SData.Client.Test.Metadata
+{
+    [TestFixture]
+    public class SDataSchemaTests
+    {
+        [Test]
+        public void Properties_Without_Types_Specified_Are_Supported_Test()
+        {
+            const string xsd = @"
+<xs:schema targetNamespace=""http://schemas.sage.com/crmErp/2008""
+           xmlns=""http://schemas.sage.com/crmErp/2008""
+           xmlns:tns=""http://schemas.sage.com/crmErp/2008"" 
+           xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+  <xs:element name=""tradingAccount"" type=""tns:tradingAccount--type"" />
+  <xs:complexType name=""tradingAccount--type"">
+    <xs:all>
+      <xs:element name=""active"" />
+    </xs:all>
+  </xs:complexType>
+</xs:schema>";
+            SDataSchema schema;
+
+            using (var reader = new StringReader(xsd))
+            {
+                schema = SDataSchema.Read(reader);
+            }
+
+            Assert.That(schema, Is.Not.Null);
+            Assert.That(schema.Resources.ContainsKey("tradingAccount"), Is.True);
+            var resource = schema.Resources["tradingAccount"];
+            Assert.That(resource, Is.Not.Null);
+            Assert.That(resource.Properties.Length, Is.EqualTo(1));
+            var property = resource.Properties[0];
+            Assert.That(property, Is.Not.Null);
+            Assert.That(property.Name, Is.EqualTo("active"));
+        }
+    }
+}
